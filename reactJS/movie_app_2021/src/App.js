b39f54b2726bdd6cb31e416de90882f80d1789cd
hayoung <chessg55@gmail.com>
2021-11-24 23:54:42 +0900

    useEffect:practice

--- reactJS/movie_app_2021/src/App.js
+++ reactJS/movie_app_2021/src/App.js
@@ -5,9 +5,9 @@ import {useState, useEffect} from "react";
 function App() {
   const [counter, setValue] = useState(0);
   const onClick = () => setValue((prev) => prev + 1);
-  console.log("I run all time");
+  console.log("I run all time.");
   // state가 변화할 때마다 re-render가 발생
-  useEffect(()=>{console.log("CALL THE API...")}, []);
+  useEffect(()=>{console.log("I run only once.")}, []);
   // state가 변화해도 더는 렌더되지 않는다.
   const [keyword, setKeyword] = useState("");
   const onChange = (event) => {
@@ -15,10 +15,13 @@ function App() {
   }
   useEffect(()=>{
     if(keyword !== "" & keyword.length > 5){
-      console.log("Search for...",keyword);
+      console.log("I run when 'keyword' changes.");
     }
   },[keyword]);
   // keyword가 변화할 때만 기능하도록. deps에 지정된 변수를 react가 watch
+  useEffect(()=>{
+    console.log("I run when 'counter' changes.");
+  }, [counter]);
   return (
     <div>
       <input value={keyword} onChange={onChange} type="text" placeholder="Search here..." />
