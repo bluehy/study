6d0e992774e10feabba87102869ff2010971c7f1
hayoung <chessg55@gmail.com>
2021-11-24 23:38:37 +0900

    useEffect를 사용해야하는 이유

--- reactJS/movie_app_2021/src/App.js
+++ reactJS/movie_app_2021/src/App.js
@@ -6,13 +6,16 @@ function App() {
   const [counter, setValue] = useState(0);
   const onClick = () => setValue((prev) => prev + 1);
   console.log("I run all time");
-  const iRunOnlyOnce = () => {
-    console.log("i run only once.");
-  }
-  useEffect(iRunOnlyOnce, []);
   // state가 변화할 때마다 re-render가 발생
+  useEffect(()=>{console.log("CALL THE API...")}, []);
+  // state가 변화해도 더는 렌더되지 않는다.
+  const [keyword, setKeyword] = useState("");
+  const onChange = (event) => {
+    setKeyword(event.target.value);
+  }
   return (
     <div>
+      <input value={keyword} onChange={onChange} type="text" placeholder="Search here..." />
       <h1 className={styles.title}>{counter}</h1>
       <Button text="Click me" onClick={onClick}>Click me</Button>
     </div>
