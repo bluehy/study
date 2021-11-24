8d77537645fb360977a32e5d1b8f3e42092a46fe
hayoung <chessg55@gmail.com>
2021-11-24 23:20:52 +0900

    useEffect: basic

--- reactJS/movie_app_2021/src/App.js
+++ reactJS/movie_app_2021/src/App.js
@@ -1,11 +1,15 @@
 import Button from "./Button.js";
 import styles from "./App.module.css";
-import {useState} from "react";
+import {useState, useEffect} from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
   const onClick = () => setValue((prev) => prev + 1);
-  console.log("render");
+  console.log("I run all time");
+  const iRunOnlyOnce = () => {
+    console.log("i run only once.");
+  }
+  useEffect(iRunOnlyOnce, []);
   // state가 변화할 때마다 re-render가 발생
   return (
     <div>
