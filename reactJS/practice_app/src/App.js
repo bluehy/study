f02319e65247a0eb75949ce6546029d5e7c3484a
hayoung <chessg55@gmail.com>
2021-11-27 20:11:12 +0900

    set up first react-app

---
+++ reactJS/practice_app/src/App.js
@@ -0,0 +1,13 @@
+import PropTypes from "prop-types";
+
+import {useState, useEffect} from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>First React</h1>
+    </div>
+  );
+}
+
+export default App;
