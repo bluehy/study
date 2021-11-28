360f1447cfc46d8ae4fe735d4c7dca5966c9d348
hayoung <chessg55@gmail.com>
2021-11-28 17:56:21 +0900

    main function_todo: input

--- reactJS/practice_app/src/App.js
+++ reactJS/practice_app/src/App.js
@@ -1,11 +1,17 @@
-import PropTypes from "prop-types";
-
-import {useState, useEffect} from "react";
+import {useState} from "react";
 
 function App() {
+  let today = new Date();
+  let year = today.getFullYear();
+  let month = today.getMonth() + 1;
+
+  const [toDo, setToDO] = useState("");
+  const onChange = (event) => setToDO(event.target.value);
+
   return (
     <div>
-      <h1>First React</h1>
+      <h1>{year}년 {month}월</h1>
+      <input onChange={onChange} value={toDo} type="text" placeholder="할 일 입력"/>
     </div>
   );
 }
