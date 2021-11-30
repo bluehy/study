93b09015070e9bc19fc1b3ee747335a85fc34fdc
hayoung <chessg55@gmail.com>
2021-11-30 23:13:52 +0900

    fetch Link

--- reactJS/coin_tracker_demo/src/App.js
+++ reactJS/coin_tracker_demo/src/App.js
@@ -1,7 +1,16 @@
-import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
-  return <div className="App"></div>;
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers");
+  }, []);
+  return (
+    <div className="App">
+      <h1>The Coins!</h1>
+      {loading ? <h2>Loading...</h2> : null}
+    </div>
+  );
 }
 
 export default App;
