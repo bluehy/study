0853b41e2f4072c5a5962d96259d5d31e805c0d1
hayoung <chessg55@gmail.com>
2021-12-01 01:09:14 +0900

    coin_tracker, converter coin

--- reactJS/coin_tracker_demo/src/App.js
+++ reactJS/coin_tracker_demo/src/App.js
@@ -3,6 +3,9 @@ import { useState, useEffect } from "react";
 function App() {
   const [loading, setLoading] = useState(true);
   const [coins, setCoins] = useState([]);
+  const [dollar, setDollar] = useState(0);
+  const [selected, setSelected] = useState(0);
+
   useEffect(() => {
     fetch("https://api.coinpaprika.com/v1/tickers")
       .then((response) => response.json())
@@ -10,22 +13,54 @@ function App() {
       .then((json) => {
         setCoins(json);
         setLoading(false);
+        // console.log(coins);
       });
   }, []);
-  console.log(coins);
+
+  const onSelect = (event) => {
+    const val = event.target.value;
+    const start = val.indexOf("$");
+    const end = val.length;
+    const coinValue = Math.abs(val.substring(start + 1, end));
+    setSelected(coinValue);
+    // https://mylko72.gitbooks.io/javascript-cookbook/content/chapter1/chapter1_2.html
+    // setSelected(event.quotes.USD.price);
+  };
+
+  const onChange = (event) => {
+    // console.log(event.target.value);
+    setDollar(event.target.value);
+  };
+
   return (
     <div className="App">
-      <h1>The Coins!</h1>
+      <h1>The Coins! ({coins.length})</h1>
       {loading ? (
         <h2>Loading...</h2>
       ) : (
-        <ul>
-          {coins.map((item) => (
-            <li>
-              {item.name} ({item.symbol}): ${item.quotes.USD.price} USD
-            </li>
-          ))}
-        </ul>
+        <form>
+          <select onChange={onSelect}>
+            {coins.map((item) => (
+              <option key={item.id}>
+                {item.name} ({item.symbol}) ${item.quotes.USD.price}
+              </option>
+            ))}
+          </select>
+          <br />
+          <input
+            onChange={onChange}
+            value={dollar}
+            type="number"
+            placeholder="input your USD"
+          />
+          <span> = </span>
+          <input
+            type="number"
+            placeholder="coin"
+            value={selected ? dollar / selected : selected}
+            disabled
+          />
+        </form>
       )}
     </div>
   );
