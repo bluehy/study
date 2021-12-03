a0cf7cb3f076d541a17306b53140971741fac938
hayoung <chessg55@gmail.com>
2021-12-03 19:42:06 +0900

    setMovies: 받아온 data로 movie object 생성

--- reactJS/movie_project_2021/src/App.js
+++ reactJS/movie_project_2021/src/App.js
@@ -2,14 +2,31 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
   useEffect(() => {
     fetch(
       "https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year"
     )
       .then((response) => response.json())
-      .then((json) => console.log(json));
+      .then((json) => {
+        setMovies(json.data.movies);
+        setLoading(false);
+      });
   }, []);
-  return <div>{loading ? <h1>Loading...</h1> : null}</div>;
+  console.log(movies);
+  return (
+    <div>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <ul>
+          {movies
+            ? movies.map((movie) => <li key={movie.id}>{movie.title}</li>)
+            : null}
+        </ul>
+      )}
+    </div>
+  );
 }
 
 export default App;
