e3d940a7bed788ccafb394e31d890dcaf33947fa
hayoung <chessg55@gmail.com>
2021-12-05 21:01:07 +0900

    movie App compnent to Home component

--- reactJS/movie_project_2021/src/App.js
+++ reactJS/movie_project_2021/src/App.js
@@ -1,50 +1,5 @@
-import { useState, useEffect } from "react";
-import Movie from "./components/Movie";
-
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-
-  const getMovies = async () => {
-    const json = await (
-      await fetch(
-        "https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year"
-      )
-    ).json();
-    // const json = await response.json();
-    setMovies(json.data.movies);
-    setLoading(false);
-  };
-
-  useEffect(() => {
-    getMovies();
-  }, []);
-  console.log(movies);
-
-  return (
-    <div>
-      <h1>High Score Movie</h1>
-      {loading ? (
-        <h2>Loading...</h2>
-      ) : (
-        <div>
-          {movies
-            ? movies.map((movie) => (
-                <Movie
-                  key={movie.id}
-                  coverImg={movie.medium_cover_image}
-                  title={movie.title}
-                  url={movie.url}
-                  title_long={movie.title_long}
-                  summary={movie.summary}
-                  genres={movie.genres}
-                />
-              ))
-            : null}
-        </div>
-      )}
-    </div>
-  );
+  return null;
 }
 
 export default App;
