1bb9679cd0753f1c859340ddce321d5be64cf38d
hayoung <chessg55@gmail.com>
2021-12-06 15:34:19 +0900

    start styled components

--- reactJS/react-for-beginners/src/App.js
+++ reactJS/react-for-beginners/src/App.js
@@ -1,7 +1,22 @@
-import "./App.css";
+import styled from "styled-components";
+
+const Parents = styled.div`
+  display: flex;
+`;
+
+const Child = styled.div`
+  width: 100px;
+  height: 100px;
+  background-color: teal;
+`;
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Parents>
+      <Child></Child>
+      <div></div>
+    </Parents>
+  );
 }
 
 export default App;
