f7093d646dafe0c4eb3961b8cf5d59c65a2d19b1
hayoung <chessg55@gmail.com>
2021-12-07 17:47:44 +0900

    use Props in TypeScript

--- reactJS/typescript-for-beginners/src/App.tsx
+++ reactJS/typescript-for-beginners/src/App.tsx
@@ -1,24 +1,12 @@
 import React from "react";
-import styled from "styled-components";
-
-const Container = styled.section`
-  width: 300px;
-  height: auto;
-  display: flex;
-  justify-content: space-between;
-`;
-const Box = styled.div`
-  width: 100px;
-  height: 100px;
-  background-color: teal;
-`;
+import Circle from "./Circle";
 
 function App() {
   return (
-    <Container>
-      <Box></Box>
-      <Box></Box>
-    </Container>
+    <div>
+      <Circle bgColor="teal" />
+      <Circle bgColor="tomato" />
+    </div>
   );
 }
 
