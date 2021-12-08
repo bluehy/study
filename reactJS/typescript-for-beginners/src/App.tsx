01d557d789a42f6b85e1d3576232a0c633df2fb0
hayoung <chessg55@gmail.com>
2021-12-08 16:20:25 +0900

    ReactFormEvent_Submit

--- reactJS/typescript-for-beginners/src/App.tsx
+++ reactJS/typescript-for-beginners/src/App.tsx
@@ -1,20 +1,22 @@
 import React from "react";
-import Circle from "./Circle";
 import { useState } from "react";
 
 function App() {
   const [value, setValue] = useState("");
   const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-    console.log(event.currentTarget.value);
+    // console.log(event.currentTarget.value);
     const {
       currentTarget: { value },
     } = event;
     setValue(value);
   };
-
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log(`hello ${value}`);
+  };
   return (
     <div>
-      <form>
+      <form onSubmit={onSubmit}>
         <input
           value={value}
           onChange={onChange}
