1e6a769d7958520cf695335ecde772be89f2eff9
hayoung <chessg55@gmail.com>
2021-12-09 16:12:50 +0900

    First Router

--- reactJS/movie_project_2021/src/App.js
+++ reactJS/movie_project_2021/src/App.js
@@ -1,5 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./routes/Home";
+
 function App() {
-  return null;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/">
+          {/* <Component/> */}
+          <Home />
+        </Route>
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
