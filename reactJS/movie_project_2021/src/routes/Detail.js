6b38c369a473a9c7f0d9a1c33fd23ecf7367a9d0
hayoung <chessg55@gmail.com>
2021-12-10 19:30:50 +0900

    Link

--- reactJS/movie_project_2021/src/routes/Detail.js
+++ reactJS/movie_project_2021/src/routes/Detail.js
@@ -1,5 +1,12 @@
+import { Link } from "react-router-dom";
+
 const Detail = () => {
-  return <h1>Detail</h1>;
+  return (
+    <div>
+      <Link to="/">Home</Link>
+      <h1>Detail</h1>
+    </div>
+  );
 };
 
 export default Detail;
