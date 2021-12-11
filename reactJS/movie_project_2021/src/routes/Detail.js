95ee7d5c469f030bf545a963d93d24674e8e7864
hayoung <chessg55@gmail.com>
2021-12-11 19:07:02 +0900

    useParams -> fetch item's data, use data, useState & useEffect

--- reactJS/movie_project_2021/src/routes/Detail.js
+++ reactJS/movie_project_2021/src/routes/Detail.js
@@ -1,13 +1,36 @@
+import { useState, useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
 
 const Detail = () => {
-  const x = useParams();
-  console.log(x);
+  const [loading, setLoading] = useState(true);
+  const [movie, setMovie] = useState([]);
+  const { id } = useParams();
+  // console.log(id);
+
+  const getMovie = async () => {
+    const json = await (
+      await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+    ).json();
+    setLoading(false);
+    setMovie(json.data.movie);
+  };
+
+  useEffect(() => {
+    getMovie();
+    console.log(movie);
+  }, []);
 
   return (
     <div>
       <Link to="/">Home</Link>
-      <h1>Detail</h1>
+      {loading ? (
+        <h2>Loading...</h2>
+      ) : (
+        <div>
+          <p>Detail Page about movie no.{id}</p>
+          <h1>{movie.title}</h1>
+        </div>
+      )}
     </div>
   );
 };
