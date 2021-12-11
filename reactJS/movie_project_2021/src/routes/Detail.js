4f2d5dacc36499db5c0aa16d777875df92d03e98
hayoung <chessg55@gmail.com>
2021-12-11 18:48:55 +0900

    how to get dynamic url's id

--- reactJS/movie_project_2021/src/routes/Detail.js
+++ reactJS/movie_project_2021/src/routes/Detail.js
@@ -1,6 +1,9 @@
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
 const Detail = () => {
+  const x = useParams();
+  console.log(x);
+
   return (
     <div>
       <Link to="/">Home</Link>
