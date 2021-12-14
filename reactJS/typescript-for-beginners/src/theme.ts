f4a9998018e2545ee7fa050cc554e4c8f78f1663
hayoung <chessg55@gmail.com>
2021-12-14 20:14:55 +0900

    theme

---
+++ reactJS/typescript-for-beginners/src/theme.ts
@@ -0,0 +1,11 @@
+import { DefaultTheme } from "styled-components";
+
+const lightTheme: DefaultTheme = {
+  bgColor: "white",
+  textColor: "black",
+};
+
+const darkTheme: DefaultTheme = {
+  bgColor: "black",
+  textColor: "white",
+};
