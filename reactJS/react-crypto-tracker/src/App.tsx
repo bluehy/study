2edf488dd5cd79ac2bf126ab52b0cb883698ac5f
hayoung <chessg55@gmail.com>
2021-12-21 17:22:41 +0900

    styled reset css link

--- reactJS/react-crypto-tracker/src/App.tsx
+++ reactJS/react-crypto-tracker/src/App.tsx
@@ -4,7 +4,7 @@ import Router from "./Router";
 
 const GlobalStyle = createGlobalStyle`
 @import url('https://fonts.googleapis.com/css2?family=Oooh+Baby&display=swap');
-
+/* reset css : https://github.com/zacanger/styled-reset/blob/master/src/index.ts */
 html, body, div, span, applet, object, iframe,
 h1, h2, h3, h4, h5, h6, p, blockquote, pre,
 a, abbr, acronym, address, big, cite, code,
