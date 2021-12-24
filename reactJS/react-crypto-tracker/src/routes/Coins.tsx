903427bb3bba5686b256f13f77bb86e947a25f16
hayoung <chessg55@gmail.com>
2021-12-24 16:53:07 +0900

    display block_클릭이 용이하게끔

--- reactJS/react-crypto-tracker/src/routes/Coins.tsx
+++ reactJS/react-crypto-tracker/src/routes/Coins.tsx
@@ -47,10 +47,10 @@ const CoinsList = styled.ul`
 const Coin = styled.li`
   background-color: white;
   color: ${(props) => props.theme.bgColor};
-  padding: 20px;
   border-radius: 15px;
   margin-bottom: 10px;
   a {
+    padding: 20px;
     transition: color 0.2s ease-in-out;
     display: block;
   }
