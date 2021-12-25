e07ef8521c937013ed20f68bd037face2f18e13f
hayoung <chessg55@gmail.com>
2021-12-25 22:05:11 +0900

    interface

--- reactJS/react-crypto-tracker/src/routes/Coins.tsx
+++ reactJS/react-crypto-tracker/src/routes/Coins.tsx
@@ -1,35 +1,6 @@
 import styled from "styled-components";
 import { Link } from "react-router-dom";
-
-const coins = [
-  {
-    id: "btc-bitcoin",
-    name: "Bitcoin",
-    symbol: "BTC",
-    rank: 1,
-    is_new: false,
-    is_active: true,
-    type: "coin",
-  },
-  {
-    id: "eth-ethereum",
-    name: "Ethereum",
-    symbol: "ETH",
-    rank: 2,
-    is_new: false,
-    is_active: true,
-    type: "coin",
-  },
-  {
-    id: "hex-hex",
-    name: "HEX",
-    symbol: "HEX",
-    rank: 3,
-    is_new: false,
-    is_active: true,
-    type: "token",
-  },
-];
+import { useState, useEffect } from "react";
 
 const Container = styled.div`
   padding: 0px 20px;
@@ -70,7 +41,19 @@ const Title = styled.h1`
   font-size: 32px;
 `;
 
+interface CoinInterface {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  is_new: boolean;
+  is_active: boolean;
+  type: string;
+}
+
 const Coins = () => {
+  const [coins, setCoins] = useState<CoinInterface[]>([]);
+
   return (
     <Container>
       <Header>
