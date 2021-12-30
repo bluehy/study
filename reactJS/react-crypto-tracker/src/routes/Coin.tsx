e6ba1ecc090dc8da7235713ba154cbd7e5035238
hayoung <chessg55@gmail.com>
2021-12-30 11:49:42 +0900

    data type, use data

--- reactJS/react-crypto-tracker/src/routes/Coin.tsx
+++ reactJS/react-crypto-tracker/src/routes/Coin.tsx
@@ -69,6 +69,27 @@ interface IPriceData {
   beta_value: number;
   first_data_at: string;
   last_updated: string;
+  quotes: {
+    USD: {
+      ath_date: string;
+      ath_price: number;
+      market_cap: number;
+      market_cap_change_24h: number;
+      percent_change_1h: number;
+      percent_change_1y: number;
+      percent_change_6h: number;
+      percent_change_7d: number;
+      percent_change_12h: number;
+      percent_change_15m: number;
+      percent_change_24h: number;
+      percent_change_30d: number;
+      percent_change_30m: number;
+      percent_from_price_ath: number;
+      price: number;
+      volume_24h: number;
+      volume_24h_change_24h: number;
+    };
+  };
 }
 
 const Coin = () => {
@@ -105,7 +126,11 @@ const Coin = () => {
       <Header>
         <Title>{name}</Title>
       </Header>
-      {loading ? <Loading /> : `${info?.rank} ${priceInfo?.id}`}
+      {loading ? (
+        <Loading />
+      ) : (
+        `${info?.rank} ${priceInfo?.id} $${priceInfo?.quotes.USD.ath_price}`
+      )}
     </Container>
   );
 };
