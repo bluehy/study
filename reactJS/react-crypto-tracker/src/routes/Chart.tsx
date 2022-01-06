2f142634e9c52e705ae1190a9cfae43524027e71
hayoung <chessg55@gmail.com>
2022-01-06 22:33:58 +0900

    invalid parameters

--- reactJS/react-crypto-tracker/src/routes/Chart.tsx
+++ reactJS/react-crypto-tracker/src/routes/Chart.tsx
@@ -10,7 +10,7 @@ const Chart = (/* { coinId }: CharProps */) => {
   // params = {coinId: "coinId"}
   //
   // Coin의 Route를 사용할 경우, Coin을 통해 props를 전달받도록 작성할 수 있음.
-  const endDate = Math.floor(Date.now()) / 1000);
+  const endDate = Math.floor(Date.now() / 1000);
   const startDate = endDate - 60 * 60 * 24 * 7;
   useEffect(() => {
     (async () => {
@@ -18,7 +18,7 @@ const Chart = (/* { coinId }: CharProps */) => {
         await fetch(
           `https://api.coinpaprika.com/v1/coins/${params.coinId}/ohlcv/historical?start=${startDate}&end=${endDate}`
         )
-        ).json();
+      ).json();
       console.log(apiData);
     })();
   }, []);
