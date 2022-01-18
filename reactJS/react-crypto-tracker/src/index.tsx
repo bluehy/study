a377f5499e31895754ad08b83185cb1eee30c649
hayoung <chessg55@gmail.com>
2022-01-18 15:31:09 +0900

    react query

--- reactJS/react-crypto-tracker/src/index.tsx
+++ reactJS/react-crypto-tracker/src/index.tsx
@@ -1,8 +1,11 @@
 import React from "react";
+import { QueryClient } from "react-query";
 import ReactDOM from "react-dom";
 import { ThemeProvider } from "styled-components";
 import App from "./App";
 import { theme, lightTheme } from "./theme";
+
+const queryClient = new QueryClient();
 
 ReactDOM.render(
   <React.StrictMode>
